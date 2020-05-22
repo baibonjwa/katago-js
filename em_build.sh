6e889d691b90f28cc0097d46a744701617b9faa0
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2020-05-22 22:14:36 +0900

    emcmake

--- em_build.sh
+++ em_build.sh
@@ -7,5 +7,5 @@ if [ ! -d $build_dir ]; then
     mkdir $build_dir
 fi
 cd $build_dir
-emconfigure cmake $src_dir -DBUILD_MCTS=1 -DUSE_BACKEND=TFJS
+emcmake cmake $src_dir -DBUILD_MCTS=1 -DUSE_BACKEND=TFJS
 emmake make
