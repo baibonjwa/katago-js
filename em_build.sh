b640b4c84e9d669ba3c4b9a79c7a93cb97f013fa
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2021-01-29 20:28:17 +0900

    does not work. loadWasmModuleToWorker seems error

--- em_build.sh
+++ em_build.sh
@@ -10,4 +10,8 @@ if [ ! -d $build_dir ]; then
     mkdir $build_dir
 fi
 cd $build_dir
-emcmake cmake $src_dir -DBUILD_MCTS=1 -DUSE_BACKEND=TFJS && emmake make
+if [ "$1" = "clean" ]; then
+    emcmake cmake $src_dir -DBUILD_MCTS=1 -DUSE_BACKEND=TFJS && emmake make clean && emmake make
+else
+    emcmake cmake $src_dir -DBUILD_MCTS=1 -DUSE_BACKEND=TFJS && emmake make
+fi
