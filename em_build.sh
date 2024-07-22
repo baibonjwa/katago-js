3c42736f2a7c96ffdd3cedd0173b4774abd3a70e
baibonjwa <baibonjwa@gmail.com>
2024-07-22 11:08:58 +0800

    Add es build

--- em_build.sh
+++ em_build.sh
@@ -9,7 +9,7 @@ version=3.1.52 # 動いたが,condition_variable#wait_forが動かない
 source emsdk_env.sh
 popd
 
-build_dir=em_build
+# build_dir=em_build
 src_dir=../cpp
 
 if [ ! -d $build_dir ]; then
@@ -24,7 +24,7 @@ fi
 
 # Build ES Modules
 # if [ "$1" = "clean" ]; then
-#     emcmake cmake $src_dir -DBUILD_MCTS=1 -DUSE_BACKEND=TFJS \
+#     emcmake cmake $src_dir -DBUILD_MCTS=1 -DUSE_BAgoproblems.developCKEND=TFJS \
 #         -DCMAKE_EXECUTABLE_SUFFIX=".js" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && \
 #     emmake make clean && \
 #     emmake make EMCC_FLAGS="-s MODULARIZE=1 -s EXPORT_ES6=1"
